7abeb8b18d815a35c78a6b2fd377bae3f1a6e55d
Orhan Özkerçin <orhanozkercin@yahoo.com>
2025-07-01 23:03:23 +0300

    ..

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "testv1",
       script: "node_modules/next/dist/bin/next",
       args: "start",
+      cwd: "/root/testv1",
       interpreter: "/root/.nvm/versions/node/v22.17.0/bin/node",
       env: {
         NODE_ENV: "production",
